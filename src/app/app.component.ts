33895420249233e2248acbab2cd29ccb6cd5b8ff
Adrián Maza <adri_mv1@hotmail.com>
2024-05-18 21:12:15 +0200

    models

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import Product from './models/Product';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +11,18 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   userName: string;
+  myNumber: number;
+  myBoolean: boolean;
+  product: Product;
 
   constructor() {
     this.userName = "Adrián";
+    this.myNumber = 10;
+    this.myBoolean = true;
+    this.product = {
+      name: "Computer",
+      price: 1000,
+      isForSale: true
+    }
   }
 }
