b916ce0ab608e83a350211a79cc4f2f6405388db
Adrián Maza <adri_mv1@hotmail.com>
2024-05-18 21:26:52 +0200

    ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NgClass, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  isDark: boolean = false;
 
+  toggleDark() {
+    this.isDark = !this.isDark
+  }
 }
