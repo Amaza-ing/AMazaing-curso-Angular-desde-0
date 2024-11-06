1296dbb4b97b10af211d0458214c67ef00a2b000
Adrián Maza <adri_mv1@hotmail.com>
2024-11-06 11:39:49 +0100

    04-ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  userName: string = 'Adrián';
   number: number = 0;
 
   addOne() {
