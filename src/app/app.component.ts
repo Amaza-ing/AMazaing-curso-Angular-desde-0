8e1b2035041b2b20ab1bf88efc3d048b5a53879b
Adrián Maza <adri_mv1@hotmail.com>
2024-11-06 12:05:00 +0100

    07-object-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,23 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  movies: string[] = ["Lord of the Rings", "Star Wars", "Dune"];
+  movies: string[] = ['Lord of the Rings', 'Star Wars', 'Dune'];
+
+  animals: any = [
+    {
+      id: 1,
+      name: 'dog',
+      img: 'https://nypost.com/wp-content/uploads/sites/2/2022/12/worlds-cutest-dog-comp-1.jpg',
+    },
+    {
+      id: 2,
+      name: 'cat',
+      img: 'https://img.freepik.com/foto-gratis/lindo-gatito-domestico-sienta-ventana-mirando-fuera-ia-generativa_188544-12519.jpg',
+    },
+    {
+      id: 3,
+      name: 'bird',
+      img: 'https://media.cnn.com/api/v1/images/stellar/prod/190414090035-01-cassowary.jpg',
+    },
+  ];
 }
---
+++ src/data/animals.js
@@ -0,0 +1,17 @@
+animals: any = [
+  {
+    id: 1,
+    name: "dog",
+    img: "https://nypost.com/wp-content/uploads/sites/2/2022/12/worlds-cutest-dog-comp-1.jpg",
+  },
+  {
+    id: 2,
+    name: "cat",
+    img: "https://img.freepik.com/foto-gratis/lindo-gatito-domestico-sienta-ventana-mirando-fuera-ia-generativa_188544-12519.jpg",
+  },
+  {
+    id: 3,
+    name: "bird",
+    img: "https://media.cnn.com/api/v1/images/stellar/prod/190414090035-01-cassowary.jpg",
+  },
+];
